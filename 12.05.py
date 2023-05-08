03bcb503e75de1f8a163cd3addac4e333777e060
hrechanyuk anastasiia <anastasiya.grk@gmail.com>
2023-05-08 20:39:16 +0300

    №4

---
+++ 12.05.py
@@ -0,0 +1,12 @@
+#4
+def log(func):
+    def wrapper(*args, **kwargs):
+        res = func(*args, **kwargs)
+        print(f"Функція: {func.__name__}, Аргументи: {args}, Результат: {res}")
+        return res
+    return wrapper
+@log
+def example_func(x, y):
+    return x + y
+print(example_func(2, 2))
+
